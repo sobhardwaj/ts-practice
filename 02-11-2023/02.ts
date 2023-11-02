42e022a21afebf81130757698d0d9035803c4e68
Siddharath  Bhardwaj <siddharathbhardwaj@gmail.com>
2023-11-02 10:09:19 -0700

    class added

---
+++ 02-11-2023/02.ts
@@ -0,0 +1,19 @@
+class DynamicPropertyClass {
+  private dynamicProperties: { [key: string]: any } = {};
+
+  setProperty(propertyName: string, value: any) {
+    this.dynamicProperties[propertyName] = value;
+  }
+
+  getProperty(propertyName: string) {
+    return this.dynamicProperties[propertyName];
+  }
+}
+
+const myObject = new DynamicPropertyClass();
+
+myObject.setProperty("property1", "Value 1");
+myObject.setProperty("property2", "Value 2");
+
+console.log(myObject.getProperty("property1")); // Output: Value 1
+console.log(myObject.getProperty("property2"));
